0591b39d608798b810b9b372c02903491fe16dad
caojie.cj <caojie.cj@antfin.com>
2018-04-11 17:45:22 +0800

    add  log

--- src/test/java/com/alipay/hessian/generic/test/PersonTest.java
+++ src/test/java/com/alipay/hessian/generic/test/PersonTest.java
@@ -200,20 +200,20 @@ public class PersonTest {
             assertEquals(null, p2.getListValue());
         } catch (Exception e) {
             e.printStackTrace();
-            System.out.println(Thread.currentThread().getName() + " current WriteDefinitionEverytimeField to "
-                    + WRITE_DEFINITION_EVERYTIME_FIELD.get(null));
+            System.out.println(Thread.currentThread().getName() + " current WriteDefinitionEverytimeField value is "
+                + WRITE_DEFINITION_EVERYTIME_FIELD.get(null));
             throw e;
         }
-        
+
     }
-    
-    private void setWriteDefinitionEverytimeField(boolean write){
+
+    private void setWriteDefinitionEverytimeField(boolean write) {
         try {
             System.out.println(Thread.currentThread().getName() + " setWriteDefinitionEverytimeField to " + write);
             WRITE_DEFINITION_EVERYTIME_FIELD.set(GenericObjectSerializer.class, write);
         } catch (IllegalAccessException e) {
             e.printStackTrace();
         }
-    } 
-    
+    }
+
 }
